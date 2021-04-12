58dd882977d24a15aef66fe0cda227a13653d2ab
Louis Lefevre <louislefev@gmail.com>
2021-04-12 22:33:50 +0100

    Removed problematic special methods
    - Removed special methods that may be problematic if not performed with particular logic. Would be more suitable to have a designated method for each.
    - Reordered special methods to be more logically ordered.
    - Updated return type of __iter__

--- inverted_index/indexing.py
+++ inverted_index/indexing.py
@@ -9,32 +9,23 @@ class InvertedIndex:
         self._index: dict[str, 'InvertedList'] = {}
         self._collection: dict[str, list[str]] = {}
 
-    def __len__(self) -> int:
-        return len(self._index)
-
-    def __getitem__(self, item: str) -> 'InvertedList':
-        return self._index[item]
-
-    def __setitem__(self, key, value):
-        self._index[key] = value
-
-    def __delitem__(self, key):
-        del self._index[key]
-
-    def __iter__(self) -> Iterable[str]:
-        return iter(self._index)
+    def __iter__(self) -> Iterable[str, 'InvertedList']:
+        return self._index.items()
 
     def __contains__(self, item) -> bool:
         return item in self._index
+
+    def __len__(self) -> int:
+        return len(self._index)
+
+    def __bool__(self) -> bool:
+        return bool(self._index)
 
     def __repr__(self) -> str:
         return repr(self._index)
 
     def __str__(self) -> str:
         return ''.join([f'{key}: {value.postings}\n' for key, value in self._index.items()])
-
-    def __bool__(self) -> bool:
-        return bool(self._index)
 
     @property
     def index(self) -> dict[str, 'InvertedList']:
